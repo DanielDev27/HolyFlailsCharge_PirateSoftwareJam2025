3b92df10e0a13c9dbccc822424190915a3e46bb9
Ontiablo <wowontiablo@gmail.com>
2025-01-20 09:21:18 +0200

    Progress on enemy wave spawn script

--- PirateSoftwareJam-Flail'sCharge/Assets/Scripts/WaveSpawningScene/WaveSpawner.cs
+++ PirateSoftwareJam-Flail'sCharge/Assets/Scripts/WaveSpawningScene/WaveSpawner.cs
@@ -1,31 +1,24 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
-    [Header("Enemies")]
-    [SerializeField] private GameObject enemySmall;
-    [SerializeField] private GameObject enemyMedium;
-    [SerializeField] private GameObject enemyHeavy;
-
     [Header("Spawn Points")]
-    [SerializeField] private Transform spawn1;
-    [SerializeField] private Transform spawn2;
-    [SerializeField] private Transform spawn3;
+    [SerializeField] public Transform spawn1;
+    [SerializeField] public Transform spawn2;
+    [SerializeField] public Transform spawn3;
 
     [Header("Countdown")]
-    [SerializeField] private float countdown;
-
-    public int spawnCost;
-
-    public List<Enemy> enemies = new List<Enemy>();
+    [SerializeField] public float countdown;
+    public List<EnemyToSpawn> enemies = new List<EnemyToSpawn>();
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
     public int currentWave;
     public int waveValue;
 
     void Start()
     {
-        
+        GenerateEnemies();
     }
 
     void FixedUpdate()
@@ -38,8 +31,21 @@ public class WaveSpawner : MonoBehaviour
         List<GameObject> generatedEnemies = new List<GameObject>();
         while (waveValue>0)
         {
-            //int randomEnemyID = Random.Range
+            int randomEnemyId = Random.Range(0, enemies.Count);
+            int randEnemyCost = enemies[randomEnemyId].spawnCost;
+
+            if(waveValue-randEnemyCost>=0)
+            {
+                generatedEnemies.Add(enemies[randomEnemyId].enemyPrefab);
+                waveValue -= randEnemyCost;
+            }
+            else if (waveValue<=0)
+            {
+                break;
+            }
         }
+        enemiesToSpawn.Clear();
+        enemiesToSpawn = generatedEnemies;
     }
 
     private void GenerateWave()
@@ -47,5 +53,12 @@ public class WaveSpawner : MonoBehaviour
         waveValue = currentWave;
         GenerateEnemies();
     }
+}
+
+[System.Serializable]
+public class EnemyToSpawn
+{
+    public GameObject enemyPrefab;
+    public int spawnCost;
 }
 
