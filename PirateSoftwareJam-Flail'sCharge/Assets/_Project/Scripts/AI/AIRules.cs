9712bc8dd22fbd4891d94b94934b9fbdb0c8b716
DanielDev27 <danielbeale27@gmail.com>
2025-01-20 14:43:22 +0200

    AI Update and moved Enemy SO to AI folder

--- PirateSoftwareJam-Flail'sCharge/Assets/_Project/Scripts/AI/AIRules.cs
+++ PirateSoftwareJam-Flail'sCharge/Assets/_Project/Scripts/AI/AIRules.cs
@@ -1,16 +1,59 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
-public class AIRules : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class AIRules : MonoBehaviour {
+    [Header ("References")]
+    [SerializeField] string aiName;
+
+    [SerializeField] public Transform aiTransform;
+    [SerializeField] public Rigidbody aiRigidBody;
+    [SerializeField] public Collider aiCollider;
+    public ActionObject currentAction;
+
+    void Awake () {
+        ValidateAndInitActionObject ();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnEnable () {
+        currentAction?.OnEnable ();
     }
+
+    void OnDisable () {
+        currentAction?.OnDisable ();
+    }
+
+    void Start () {
+        RegisterAndConfigureActionObject ();
+        currentAction?.Start ();
+    }
+
+    void Update () { }
+
+    void FixedUpdate () {
+        currentAction?.FixedUpdate ();
+    }
+
+    void OnDrawGizmos () {
+        currentAction?.OnDrawGizmos ();
+    }
+
+    void ValidateAndInitActionObject () {
+        if (currentAction == null) {
+            Debug.Log ($"{gameObject.name} disabled");
+            gameObject.SetActive (false);
+            return;
+        }
+
+        currentAction = Instantiate (currentAction);
+        Debug.Log ($"{gameObject.name} {currentAction.GetInstanceID ()} instantiated");
+        currentAction?.Awake ();
+    }
+
+    void RegisterAndConfigureActionObject () {
+        if (currentAction != null) { }
+    }
+
+    void OnCollisionEnter (Collision other) { }
 }
