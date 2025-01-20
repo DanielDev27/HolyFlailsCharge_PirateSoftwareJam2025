f3ed98f311e7f9bb332313607fb9a5c42f0166d6
Ontiablo <wowontiablo@gmail.com>
2025-01-20 12:49:33 +0200

    Further Iterations on Enemy WaveSpawner Script
    I've added some more iterations on the enemy WaveSpawner script
    I've gone and added a bunch of comments explaining what the code does
    I've also added a few keypresses for debugging to make sure it works the way I want it to work
    A bit more work needs to be done but otherwise it's almost ready

--- PirateSoftwareJam-Flail'sCharge/Assets/_Project/Scripts/WaveSpawningScene/WaveSpawner.cs
+++ PirateSoftwareJam-Flail'sCharge/Assets/_Project/Scripts/WaveSpawningScene/WaveSpawner.cs
@@ -14,6 +14,7 @@ public class WaveSpawner : MonoBehaviour
     [SerializeField] public float countdown = 8f; // This is the time between waves
     [SerializeField] public int enemiesPerWave; 
     [SerializeField] public int testEnemiesToSpawn = 3;
+    [SerializeField] public float enemySpawnDelay = 1.5f;
     [SerializeField] public float spawnVariance = 3f; // This will control the maximum distance between spawns of the spawn point. This way the enemy doesn't spawn on once another.
 
     private float countdownTimer;
@@ -23,44 +24,53 @@ public class WaveSpawner : MonoBehaviour
 
     void Start()
     {
-        // Initialize the countdown timer
         countdownTimer = countdown;
-
-        enemiesPerWave = currentWave + 2;
     }
     
 
     void FixedUpdate()
-    {
-        // Checking if there is no wave in progress and if there are no enemies alive before I run the code
-        if (!waveInProgress && enemiesAlive == 0)
+    { 
+        if (!waveInProgress && enemiesAlive == 0)       // Checking if there is no wave in progress and if there are no enemies alive before I run the code
         {
-            countdownTimer -= Time.deltaTime;
-            if (countdownTimer <= 0)
-            {
-                GenerateWave();
-                waveInProgress = true; 
-                countdownTimer = countdown; 
-            }
+            NextWave();
+        }
+    }
+
+    private void NextWave()
+    {
+        countdownTimer -= Time.deltaTime;
+        if (countdownTimer <= 0)
+        {
+            Debug.Log("The current wave number is: " + currentWave);
+            enemiesPerWave = currentWave + 2;
+            Debug.Log("Spawning " + enemiesPerWave + " enemies");
+            GenerateWave();
+            waveInProgress = true;
+            countdownTimer = countdown;
+            currentWave++;
         }
     }
 
     private void GenerateWave()
     {
-        // Spawn the specified number of enemies for the wave
-        for (int i = 0; i < enemiesPerWave; i++)
+        StartCoroutine(SpawnEnemiesWithDelay());     // Start the coroutine to spawn enemies with a delay
+    }
+
+    private IEnumerator SpawnEnemiesWithDelay()
+    {
+        for (int i = 0; i < enemiesPerWave; i++)          // Spawn the specified number of enemies for the wave
         {
             SpawnEnemy();
+            yield return new WaitForSeconds(enemySpawnDelay);   // Wait for a specific amount of time before spawning the next enemy
         }
     }
+
     private void SpawnEnemy()
     {
-        // This randomly selects a prefab from the array
-        int randomEnemyIndex = Random.Range(0, enemyPrefabs.Length);
+        int randomEnemyIndex = Random.Range(0, enemyPrefabs.Length);          // This randomly selects a prefab from the array
         GameObject randomEnemy = enemyPrefabs[randomEnemyIndex];
 
-        // This randomly selects a spawn point from the array
-        int randomSpawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int randomSpawnPointIndex = Random.Range(0, spawnPoints.Length);      // This randomly selects a spawn point from the array
         Transform randomSpawnPoint = spawnPoints[randomSpawnPointIndex];
 
         Vector3 spawnPosition = randomSpawnPoint.position + new Vector3(
@@ -69,17 +79,20 @@ public class WaveSpawner : MonoBehaviour
             Random.Range(-spawnVariance, spawnVariance)
         );
 
-        // This then spawns the enemy at the position of the randomly selected spawn point and assigns that enemy the variable "spawnedEnemy"
-        GameObject spawnedEnemy = Instantiate(randomEnemy, spawnPosition, randomSpawnPoint.rotation);
+        GameObject spawnedEnemy = Instantiate(randomEnemy, spawnPosition, randomSpawnPoint.rotation); // This then spawns the enemy at the position of the randomly selected 
         enemiesAlive++;
+        Debug.Log("There are " + enemiesAlive + " enemies alive");                                                                               // spawn point and assigns that enemy the variable "spawnedEnemy"
 
-        // This code will detect when an enemy dies, which will then call on the HandleEnemyDeath method
-        // spawnedEnemy.GetComponent<Enemy>().OnDeath += HandleEnemyDeath;
+        // need to reference an enemy death here that will call on the method HandleEnemyDeath()
     }
 
     private void HandleEnemyDeath()
     {
         enemiesAlive--;
+
+        Debug.Log("There are " + enemiesAlive + " enemies alive");
+
+        if(enemiesAlive< 0) enemiesAlive = 0; // just trying to make sure it never goes into the negative for whatever reason
 
         if (enemiesAlive == 0)
         {
@@ -94,6 +107,16 @@ public class WaveSpawner : MonoBehaviour
             Debug.Log("M key pressed! Spawning test enemies...");
             SpawnTestEnemies(); // Call the test spawn function
         }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("N key pressed! Moving to next wave...");
+            enemiesAlive = 1; //forces HandleEnemyDeath() to succeed
+            HandleEnemyDeath();
+        }
+        if (Input.GetKeyDown(KeyCode.C)) // Debugging to see what will happen when all enemies are dead
+        {
+            CullEnemies();
+        }
     }
     private void SpawnTestEnemies()
     {
@@ -102,8 +125,19 @@ public class WaveSpawner : MonoBehaviour
             SpawnEnemy();
         }
     }
+    private void CullEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy"); // Find all game objects with the tag "Enemy"
 
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy); // Destroy each enemy
+        }
 
+        enemiesAlive = 0; // Reset the enemiesAlive counter
+        waveInProgress = false; // Reset the wave progress flag
+        Debug.Log("All enemies have been culled.");
+    }
 }
 
 
