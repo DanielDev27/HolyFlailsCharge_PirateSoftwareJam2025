d5f923bc2f6345e464e0d138980eb67813323a25
DanielDev27 <danielbeale27@gmail.com>
2025-01-27 10:09:59 +0200

    Fixed some bugs with the AI and added Audio Listener to Game Scene

--- PirateSoftwareJam-Flail'sCharge/Assets/_Project/Scripts/Health Systems/Health.cs
+++ PirateSoftwareJam-Flail'sCharge/Assets/_Project/Scripts/Health Systems/Health.cs
@@ -23,8 +23,8 @@ public class Health : MonoBehaviour {
     //[HideInInspector] public bool OnCooldown = false;
     public float damageCooldownTime;
 
-    //bool _coroutineActivated = false;
-    //public bool IsInvincible = false;
+    bool _coroutineActivated = false;
+    public bool IsInvincible = false;
 
     public static UnityEvent onDeath;
     public static UnityEvent<int> onHit;
@@ -63,12 +63,19 @@ public class Health : MonoBehaviour {
         waveSpawner = FindFirstObjectByType<WaveSpawner> ();
     }
 
-    void OnEnable () { }
+    void OnEnable () { IsInvincible = false; }
 
     void Start () {
         ResetHealth ();
         if (!Player) {
             healthView.UpdateHealthValue ((float) currentHp / (float) maxHp);
+        }
+    }
+
+    public void ResetHealth () {
+        currentHp = maxHp;
+        if (Player) {
+            HUD.instance?.UpdateDisplayedHealth (currentHp);
         }
     }
 
@@ -82,11 +89,17 @@ public class Health : MonoBehaviour {
             HUD.instance?.UpdateDisplayedHealth (currentHp);
         } else {
             //if (OnCooldown) return;
-            //if (IsInvincible) return;
             if (isDying) return;
-            currentHp -= (damageAmount);
-            healthView.UpdateHealthValue ((float) currentHp / (float) maxHp);
-            //StartCoroutine (DamageCooldown ());
+            switch (IsInvincible) {
+                case true:
+                    return;
+                case false:
+                    IsInvincible = true;
+                    StartCoroutine (DamageCooldown ());
+                    currentHp -= (damageAmount);
+                    healthView.UpdateHealthValue ((float) currentHp / (float) maxHp);
+                    break;
+            }
         }
 
         if (currentHp <= 0 && isDying == false) //Activate death coroutine if hp is below or equal to 0 and if entity isnt already dying
@@ -95,10 +108,14 @@ public class Health : MonoBehaviour {
         }
     }
 
-    public void ResetHealth () {
-        currentHp = maxHp;
-        if (Player) {
-            HUD.instance?.UpdateDisplayedHealth (currentHp);
+
+    public IEnumerator DamageCooldown () //Use this to give Invulnerability frames if needed.
+    {
+        if (!_coroutineActivated) {
+            _coroutineActivated = true;
+            yield return new WaitForSeconds (damageCooldownTime);
+            IsInvincible = false;
+            _coroutineActivated = false;
         }
     }
 
@@ -111,17 +128,4 @@ public class Health : MonoBehaviour {
             playerController.Death ();
         }
     }
-
-    /*public IEnumerator DamageCooldown () //Use this to give Invulnerability frames if needed.
-    {
-        if (!_coroutineActivated) {
-            _coroutineActivated = true;
-            OnCooldown = true;
-
-            yield return new WaitForSeconds (damageCooldownTime);
-
-            OnCooldown = false;
-            _coroutineActivated = false;
-        }
-    }*/
 }
